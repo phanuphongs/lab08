479d82fa60002dad05d26573f396f28436ac337b
IC <ic.kmitl@gmail.com>
2016-03-04 18:03:35 +0700

    hh

--- lab08/lab08/drawing1.py
+++ lab08/lab08/drawing1.py
@@ -1,3 +1,34 @@
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple drawing")
+<<<<<<< HEAD
+ 
+=======
+
+>>>>>>> origin/master
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+                       QPoint(70, 100), QPoint(100,110),
+                       QPoint(130, 100), QPoint(100, 150)
+                       ])
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+
+        p.drawPolygon([
+                       QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
+                       ])
+
+<<<<<<< HEAD
+        p.end()
+
+
 class Simple_drawing_window1(Simple_drawing_window):
     def __init__(self):
         QWidget.__init__(self, None)
@@ -8,25 +39,20 @@ class Simple_drawing_window1(Simple_drawing_window):
         p = QPainter()
         p.begin(self)
 
-        p.setBrush(QColor(0, 127, 0))
-        p.drawRect([
-                       QPoint(70, 100), QPoint(100,110),
-                       QPoint(130, 100), QPoint(100, 150),
-                       ])
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
+        p.setBrush(QColor(102, 255, 178))
+        p.drawRect(75, 200, 50, 50)
+        p.setPen(QColor(0, 0, 255))
+        p.setBrush(QColor(255, 0, 0))
         p.drawPie(50, 150, 100, 100, 0, 180 * 16)
 
-        p.drawRect([
-                       QPoint(50, 200), QPoint(150, 200), QPoint(100, 400), 
-                       ])
+    
         
-        p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
+     
         p.end()
 
 
 def main():
-    app = QApplication(sys.argo)
+    app = QApplication(sys.argv)
     w = Simple_drawing_window1()
     w.show()
 
@@ -34,3 +60,7 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
+=======
+
+        p.end()
+>>>>>>> origin/master
